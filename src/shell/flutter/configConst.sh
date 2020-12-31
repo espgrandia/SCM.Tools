ae31f7f2421f0f1f00d45de8be30f563b85fd9d2
esp <espgrandia78@hotmail.com>
2020-12-31 12:08:45 +0800

    [feat] [新增 configConst.sh 的內容]
    # [feat] [新增 configConst.sh 的內容]
    
    - 新增 build config type 對應的 const
    - 新增 target platform 發佈平台的 const

--- src/shell/flutter/configConst.sh
+++ src/shell/flutter/configConst.sh
@@ -18,6 +18,15 @@ export configConst_Subcommand_ios="ios"
 export configConst_Subcommand_ios_framework="ios-framework"
 ## ================================== subcommand key section : End ==================================
 
+## ================================== buildConfigType key section : Begin ==================================
+# subcommand : [apk] [appbundle] [bundle] [ios] 支援。
+# 依據 flutter build ， 有 debug ， profile ， release
+configConst_BuildConfigType_Debug=debug
+configConst_BuildConfigType_Profile=profile
+configConst_BuildConfigType_Release=release
+
+## ================================== buildConfigType key section : End ==================================
+
 ## ================================== ToogleFeature key section : Begin ==================================
 # @brief 針對 flutter build [subcommand] 的後續參數，屬於開關性質的設定。
 # @details 
@@ -29,19 +38,27 @@ export configConst_Subcommand_ios_framework="ios-framework"
 export configConst_BuildParam_ToggleFeature_NoPub="no-pub"
 ## ================================== ToogleFeature key section : End ==================================
 
-## ================================== ToogleFeature key section : Begin ==================================
+## ================================== Parm that needed contains value section : Begin ==================================
 # @brief 針對 flutter build [subcommand] 的後續參數，屬於需要額外帶入內容的項目。
 # @details 
 #  - optional 的功能。
 #  - confitTools.sh 會用到此區塊的 key，使用者只需要呼叫對應的函式即可。
 #  - exported.sh 會依照 build config 的相關設定來處理。
 
-# 設定 dart 的環境變數，可於 dart 層取得。
+# [dart-define] 設定 dart 的環境變數，可於 dart 層取得。
 export configConst_BuildParam_Key_DartDefine="dart-define"
 
-# 依照 flavor 來做編譯，Android 為 flavor， iOS 為 Xcode schemes。
+# [flavor] 依照 flavor 來做編譯，Android 為 flavor， iOS 為 Xcode schemes。
 export configConst_BuildParam_Key_flavor="flavor"
 
-# 設定 Android 的發佈平台。
+# [target-platform] 設定 Android 的發佈平台。
 export configConst_BuildParam_Key_TargetPlatform="target-platform"
-## ================================== ToogleFeature key section : End ==================================
+
+# [target-platform] [value] Android 的發佈平台可設定的 value [android-arm (default), android-arm64 (default), android-x86, android-x64 (default)]
+export configConst_BuildParam_TargetPlatform_arm="android-arm"
+export configConst_BuildParam_TargetPlatform_arm64="android-arm64"
+export configConst_BuildParam_TargetPlatform_x86="android-x86"
+export configConst_BuildParam_TargetPlatform_x64="android-x64"
+## ================================== Parm that needed contains value section : End ==================================
+
+
